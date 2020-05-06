5fabf3919fa4d46c343284e39edddadf379c6c9a
Jibbeb <35456650+Jibbeb@users.noreply.github.com>
2020-05-06 14:00:44 +0200

    Opmaak 2

--- ProjectBedrijfApp/Bestellen.aspx
+++ ProjectBedrijfApp/Bestellen.aspx
@@ -8,7 +8,7 @@
 <style>
     #container{
         height: 550px;
-        width: 900px;
+        width: 910px;
     }
 
 
@@ -16,6 +16,8 @@
     #topbalk {
         height: 25px;
         width: 900px;
+        border: 1px;
+        border-style: solid;
     }
     #datum {
         height: 25px;
@@ -47,6 +49,14 @@
     }    
     #menuselectie {
         height: 25px;
+        width: 900px;
+        border: 1px;
+        border-top: 0px;
+        border-style: solid;
+    }
+
+    #ddlMenulijst{
+        margin: 1px;
     }
 
     /* menu's */
@@ -72,14 +82,52 @@
         height: 475px;
         width: 740px;
         float: left;
+        border: 1px;
+        border-top: 0px;
+        border-style: solid;
     }
     
 
     /* bestelvenster */
     #bestelvenster{
         float: left;
-        width: 160px; 
+        width: 159px; 
         height: 475px;
+        border-left-style: solid;
+        border-left-color: inherit;
+        border-left-width: 0px;
+        border-right-style: solid;
+        border-right-color: inherit;
+        border-right-width: 1px;
+        border-top-style: solid;
+        border-top-color: inherit;
+        border-top-width: 0px;
+        border-bottom-style: solid;
+        border-bottom-color: inherit;
+        border-bottom-width: 1px;        
+    }
+
+    #bestelvenster2{
+        width: 154px; 
+        height: 470px;
+        margin: 5px
+    }
+
+    .toegevoegd{
+        float: left;
+        margin: 0px
+    }
+
+    .aantal{
+        float: right;
+    }
+
+    .plus{
+        float: right;
+    }
+
+    .minus{
+        float: right;
     }
     
 </style>
@@ -102,7 +150,7 @@
                 <!-- menuselectie -->
                 <div id="menuselectie">
                     <div id="menulijst">
-                        <asp:DropDownList ID="ddlMenulijst" runat="server" Height="25px" Width="300px">
+                        <asp:DropDownList ID="ddlMenulijst" runat="server" Height="23px" Width="298px">
                             <asp:ListItem>Soepen</asp:ListItem>
                             <asp:ListItem>Sushi</asp:ListItem>
                             <asp:ListItem>Vlees</asp:ListItem>
@@ -173,7 +221,64 @@
 
                 <!-- bestelvenster -->
                 <div id="bestelvenster">
-                    <asp:Button ID="btnBestel" runat="server" Text="BESTEL" Height="50px" Width="160px" />
+                    <div id="bestelvenster2">
+                        <asp:Button ID="btnBestel" runat="server" Text="BESTEL" Height="50px" Width="150px" />                      
+                        <br/>
+                        <br/>
+                        <asp:Label class="toegevoegd" ID="lblToegevoegd1" runat="server" Text="Gerecht1"></asp:Label>                        
+                        <asp:Button class="plus" ID="btnPlus1" runat="server" Height="16px" Text="+" Width="16px" />
+                        <asp:Button class="minus" ID="btnMinus1" runat="server" Height="16px" Text="-" Width="16px" />
+                        <asp:Label class="aantal" ID="lblAantal1" runat="server" Text="1"></asp:Label>
+                        <br/>
+                        <br/>
+                        <asp:Label class="toegevoegd" ID="lblToegevoegd2" runat="server" Text="Gerecht1"></asp:Label>                        
+                        <asp:Button class="plus" ID="btnPlus2" runat="server" Height="16px" Text="+" Width="16px" />
+                        <asp:Button class="minus" ID="btnMinus2" runat="server" Height="16px" Text="-" Width="16px" />
+                        <asp:Label class="aantal" ID="lblAantal2" runat="server" Text="1"></asp:Label>
+                        <br/>
+                        <br/>
+                        <asp:Label class="toegevoegd" ID="lblToegevoegd3" runat="server" Text="Gerecht1"></asp:Label>                        
+                        <asp:Button class="plus" ID="btnPlus3" runat="server" Height="16px" Text="+" Width="16px" />
+                        <asp:Button class="minus" ID="btnMinus3" runat="server" Height="16px" Text="-" Width="16px" />
+                        <asp:Label class="aantal" ID="lblAantal3" runat="server" Text="1"></asp:Label>
+                        <br/>
+                        <br/>
+                        <asp:Label class="toegevoegd" ID="lblToegevoegd4" runat="server" Text="Gerecht1"></asp:Label>                        
+                        <asp:Button class="plus" ID="btnPlus4" runat="server" Height="16px" Text="+" Width="16px" />
+                        <asp:Button class="minus" ID="btnMinus4" runat="server" Height="16px" Text="-" Width="16px" />
+                        <asp:Label class="aantal" ID="lblAantal4" runat="server" Text="1"></asp:Label>
+                        <br/>
+                        <br/>
+                        <asp:Label class="toegevoegd" ID="lblToegevoegd5" runat="server" Text="Gerecht1"></asp:Label>                        
+                        <asp:Button class="plus" ID="btnPlus5" runat="server" Height="16px" Text="+" Width="16px" />
+                        <asp:Button class="minus" ID="btnMinus5" runat="server" Height="16px" Text="-" Width="16px" />
+                        <asp:Label class="aantal" ID="lblAantal5" runat="server" Text="1"></asp:Label>
+                        <br/>
+                        <br/>
+                        <asp:Label class="toegevoegd" ID="lblToegevoegd6" runat="server" Text="Gerecht1"></asp:Label>                        
+                        <asp:Button class="plus" ID="btnPlus6" runat="server" Height="16px" Text="+" Width="16px" />
+                        <asp:Button class="minus" ID="btnMinus6" runat="server" Height="16px" Text="-" Width="16px" />
+                        <asp:Label class="aantal" ID="lblAantal6" runat="server" Text="1"></asp:Label>
+                        <br />
+                        <br/>
+                        <asp:Label class="toegevoegd" ID="lblToegevoegd7" runat="server" Text="Gerecht1"></asp:Label>                        
+                        <asp:Button class="plus" ID="btnPlus7" runat="server" Height="16px" Text="+" Width="16px" />
+                        <asp:Button class="minus" ID="btnMinus7" runat="server" Height="16px" Text="-" Width="16px" />
+                        <asp:Label class="aantal" ID="lblAantal7" runat="server" Text="1"></asp:Label>
+                        <br />
+                        <br/>
+                        <asp:Label class="toegevoegd" ID="lblToegevoegd8" runat="server" Text="Gerecht1"></asp:Label>                        
+                        <asp:Button class="plus" ID="btnPlus8" runat="server" Height="16px" Text="+" Width="16px" />
+                        <asp:Button class="minus" ID="btnMinus8" runat="server" Height="16px" Text="-" Width="16px" />
+                        <asp:Label class="aantal" ID="lblAantal8" runat="server" Text="1"></asp:Label>
+                        <br />
+                        <br/>
+                        <asp:Label class="toegevoegd" ID="lblToegevoegd9" runat="server" Text="Gerecht1"></asp:Label>                        
+                        <asp:Button class="plus" ID="btnPlus9" runat="server" Height="16px" Text="+" Width="16px" />
+                        <asp:Button class="minus" ID="btnMinus9" runat="server" Height="16px" Text="-" Width="16px" />
+                        <asp:Label class="aantal" ID="lblAantal9" runat="server" Text="1"></asp:Label>
+                        <br />
+                    </div>
                 </div>
             </div>
         </div>
--- ProjectBedrijfApp/Bestellen.aspx.designer.cs
+++ ProjectBedrijfApp/Bestellen.aspx.designer.cs
@@ -184,5 +184,329 @@ namespace ProjectBedrijfApp
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnBestel;
+
+        /// <summary>
+        /// lblToegevoegd1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblToegevoegd1;
+
+        /// <summary>
+        /// btnPlus1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus1;
+
+        /// <summary>
+        /// btnMinus1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus1;
+
+        /// <summary>
+        /// lblAantal1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAantal1;
+
+        /// <summary>
+        /// lblToegevoegd2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblToegevoegd2;
+
+        /// <summary>
+        /// btnPlus2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus2;
+
+        /// <summary>
+        /// btnMinus2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus2;
+
+        /// <summary>
+        /// lblAantal2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAantal2;
+
+        /// <summary>
+        /// lblToegevoegd3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblToegevoegd3;
+
+        /// <summary>
+        /// btnPlus3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus3;
+
+        /// <summary>
+        /// btnMinus3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus3;
+
+        /// <summary>
+        /// lblAantal3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAantal3;
+
+        /// <summary>
+        /// lblToegevoegd4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblToegevoegd4;
+
+        /// <summary>
+        /// btnPlus4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus4;
+
+        /// <summary>
+        /// btnMinus4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus4;
+
+        /// <summary>
+        /// lblAantal4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAantal4;
+
+        /// <summary>
+        /// lblToegevoegd5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblToegevoegd5;
+
+        /// <summary>
+        /// btnPlus5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus5;
+
+        /// <summary>
+        /// btnMinus5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus5;
+
+        /// <summary>
+        /// lblAantal5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAantal5;
+
+        /// <summary>
+        /// lblToegevoegd6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblToegevoegd6;
+
+        /// <summary>
+        /// btnPlus6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus6;
+
+        /// <summary>
+        /// btnMinus6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus6;
+
+        /// <summary>
+        /// lblAantal6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAantal6;
+
+        /// <summary>
+        /// lblToegevoegd7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblToegevoegd7;
+
+        /// <summary>
+        /// btnPlus7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus7;
+
+        /// <summary>
+        /// btnMinus7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus7;
+
+        /// <summary>
+        /// lblAantal7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAantal7;
+
+        /// <summary>
+        /// lblToegevoegd8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblToegevoegd8;
+
+        /// <summary>
+        /// btnPlus8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus8;
+
+        /// <summary>
+        /// btnMinus8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus8;
+
+        /// <summary>
+        /// lblAantal8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAantal8;
+
+        /// <summary>
+        /// lblToegevoegd9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblToegevoegd9;
+
+        /// <summary>
+        /// btnPlus9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus9;
+
+        /// <summary>
+        /// btnMinus9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus9;
+
+        /// <summary>
+        /// lblAantal9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAantal9;
     }
 }
