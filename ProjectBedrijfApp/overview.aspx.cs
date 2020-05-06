e9a02d993e3ac1bd4c6b43f7f63fc5e0dbda7502
RosaHarris2905 <rosaharris@live.nl>
2020-05-06 13:56:32 +0200

    overview en tablet keuze

---
+++ ProjectBedrijfApp/overview.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectBedrijfApp
+{
+    public partial class overview1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void t1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void t2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void t3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void t5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void t6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void t7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void t8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button24_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void t4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
